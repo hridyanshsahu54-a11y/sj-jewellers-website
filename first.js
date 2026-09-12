7d1111959f03da7bf9542692cc729ab83eed3fd1
hridyanshsahu54-a11y <hridyanshsahu54@gmail.com>
2026-09-12 21:24:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,25 @@
+console.log("Sahu Jewellers website started!");
+console.log("Welcome Hridyansh!");
+
+
+// Collection button
+function showMessage(collection) {
+
+    alert(
+        "Welcome to Sahu Jewellers!\n\n" +
+        collection +
+        " collection will be available soon."
+    );
+
+}
+
+
+// Contact button
+function contactUs() {
+
+    alert(
+        "Thank you for choosing Sahu Jewellers!\n\n" +
+        "Please visit our store or contact us for more information."
+    );
+
+}
